00932bc0aabc94ae42f287b68503f60c1ea32720
Helder Jordão <helder.jordao@dtx-colab.pt>
2022-06-02 13:43:08 +0100

    mv-1 #time 40m added api configs on application.properties

--- src/main/java/com/java/marvel/MarvelApplication.java
+++ src/main/java/com/java/marvel/MarvelApplication.java
@@ -1,13 +1,15 @@
 package com.java.marvel;
 
-import com.java.marvel.repository.ItemRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import com.java.marvel.repository.ItemRepository;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -16,12 +18,18 @@ public class MarvelApplication {
 	@Autowired
 	ItemRepository marvelCharactersRepo;
 
+	@Value("${api.timeout}")
+	private int apiTimeout;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MarvelApplication.class, args);
 	}
 
 	@Bean
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		var factory = new SimpleClientHttpRequestFactory();
+		factory.setConnectTimeout(apiTimeout);
+		factory.setReadTimeout(apiTimeout);
+		return new RestTemplate(factory);
 	}
 }
--- src/main/java/com/java/marvel/controller/ConsumeMarvelAPI.java
+++ src/main/java/com/java/marvel/controller/ConsumeMarvelAPI.java
@@ -2,10 +2,8 @@ package com.java.marvel.controller;
 
 import java.util.Arrays;
 
-import com.java.marvel.model.CharacterItem;
-
-import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -20,15 +18,19 @@ public class ConsumeMarvelAPI {
     @Autowired
     RestTemplate restTemplate;
 
+    @Value("${api.url}")
+    private String apiUrl;
+
     @RequestMapping(value = "/characters")
     public String getCharactersList() {
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<String> entity = new HttpEntity<String>(headers);
-        String url = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=9412d02c19994d916e56ec1700a9ff74&hash=431140e0f8d521ac0347d573ffc357d2";
 
-        return restTemplate.exchange(
-                url,
+        String response = restTemplate.exchange(
+                apiUrl,
                 HttpMethod.GET, entity, String.class).getBody();
+        return response;
+
     }
 }
